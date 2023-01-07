df6f743ee3ec13a1de8f359d1eaa95e3bcf1d092
Roman V. Isaev <rm@aroma.ru>
2023-01-07 17:36:08 +0300

    bash script cleanup

--- test.sh
+++ test.sh
@@ -3,35 +3,35 @@
 mkdir /tmp/ftp1
 mkdir /tmp/ftp2
 
-echo Start first FTP server on port 2121
+echo Starting first FTP server on port 2121
 python3 -m pyftpdlib -p 2121 -u u1 -P p1 -d /tmp/ftp1 -w &
 ftp1_pid=$!
 
-echo Start second FTP server on port 2122
+echo Starting second FTP server on port 2122
 python3 -m pyftpdlib -p 2122 -u u2 -P p2 -d /tmp/ftp2 -w &
 ftp2_pid=$!
 
 echo pythnons
 ps axuww|grep python
-echo Generate some files in the first servers directory
+echo Generating some files in the first servers directory
 echo "test1" > /tmp/ftp1/test1.txt
 echo "test2" > /tmp/ftp1/test2.txt
 echo "test3" > /tmp/ftp1/test3.txt
 
-echo Create config file for iftpfm2
+echo Creating config file for iftpfm2, age is 1 second
 echo "localhost,2121,u1,p1,/,localhost,2122,u2,p2,/,1" > /tmp/config.txt
 
+echo Waiting 2 seconds to expire the age
 sleep 2
-echo Run iftpfm2 without file pattern so it uses default ".*\.txt"
+
+echo Running iftpfm2 without file pattern so it uses default ".*\.xml", resulting in no transfers for txt files
 ./target/debug/iftpfm2 /tmp/config.txt
 
-echo Run iftpfm2 using the config file, the -d option to delete source files and -x to match txt files
+echo Running iftpfm2 using the config file, the -d option to delete source files and -x to match txt files
 ./target/debug/iftpfm2 -d -x ".*\.txt" /tmp/config.txt
 
 echo Ensure that the files were moved to the second servers directory and deleted from the source server
-
 echo
-
 if [ -f "/tmp/ftp2/test1.txt" ] && [ -f "/tmp/ftp2/test2.txt" ] && [ -f "/tmp/ftp2/test3.txt" ] && [ ! -f "/tmp/ftp1/test1.txt" ] && [ ! -f "/tmp/ftp1/test2.txt" ] && [ ! -f "/tmp/ftp1/test3.txt" ]; then
     echo "SUCCESS: files transferred and deleted as expected"
 else
@@ -40,7 +40,7 @@ fi
 
 echo
 
-echo Stop the FTP servers
+echo Cleanup
 
 kill $ftp1_pid
 kill $ftp2_pid
